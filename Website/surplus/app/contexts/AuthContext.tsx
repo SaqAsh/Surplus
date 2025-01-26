2416479468524bd46c2fe2956e1170be89fb02b7
andytluminosity <andytong369@gmail.com>
2025-01-25 21:41:16 -0500

    Fixed Firebase Auth

--- Website/surplus/app/contexts/AuthContext.tsx
+++ Website/surplus/app/contexts/AuthContext.tsx
@@ -2,6 +2,7 @@
 
 import { createContext, useContext, useState, useEffect, ReactNode } from 'react';
 import { getAuth, onAuthStateChanged, User, signOut } from 'firebase/auth';
+import { initializeApp } from 'firebase/app';
 
 interface AuthContextType {
   user: User | null;
@@ -18,7 +19,21 @@ const AuthContext = createContext<AuthContextType>({
 export function AuthProvider({ children }: { children: ReactNode }) {
   const [user, setUser] = useState<User | null>(null);
   const [loading, setLoading] = useState(true);
-  const auth = getAuth();
+
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyBZHavEWbWJikh20WOZMLkWs2beoz_TPzE",
+    authDomain: "surplus-aed79.firebaseapp.com",
+    projectId: "surplus-aed79",
+    storageBucket: "surplus-aed79.firebasestorage.app",
+    messagingSenderId: "992402453671",
+    appId: "1:992402453671:web:0e9b0a92bc9ce7167c5782",
+    measurementId: "G-3KJHKHQJXW"
+  };
+  
+  // Initialize Firebase
+  const firebaseApp = initializeApp(firebaseConfig);
+  const auth = getAuth(firebaseApp);
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
